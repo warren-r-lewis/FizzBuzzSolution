a0dd9ac11a5ced93d670979a48e8d3c5e607ed7f
Warren Lewis <warrenrlewis95@gmail.com>
2021-11-29 16:46:18 -0500

    Add project files.

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FizzBuzz;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        for (int num =1; num <= 100; num++)
+        {
+            if (num % 3 == 0)
+            {
+                if (num % 15 == 0)
+                {
+                    Console.WriteLine("Fizz Buzz");
+                }
+                else { Console.WriteLine("Fizz"); }
+
+            }
+            else if (num % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else { Console.WriteLine(num); }
+        }
+
+    }
+}
+
